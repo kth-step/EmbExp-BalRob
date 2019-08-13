836f5c06e966750739428f380ed210723f645c2b
Andreas Lindner <andreas.lindner@a-eye.se>
2019-08-14 01:50:26 +0200

    add remote program

---
+++ RemoteControl/RemoteControl/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace RemoteControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_connect = new System.Windows.Forms.Button();
+            this.listBox_events = new System.Windows.Forms.ListBox();
+            this.button_motor = new System.Windows.Forms.Button();
+            this.label_angleVal = new System.Windows.Forms.Label();
+            this.trackBar_kp = new System.Windows.Forms.TrackBar();
+            this.trackBar_ki = new System.Windows.Forms.TrackBar();
+            this.trackBar_kd = new System.Windows.Forms.TrackBar();
+            this.label_kp_val = new System.Windows.Forms.Label();
+            this.label_ki_val = new System.Windows.Forms.Label();
+            this.label_kd_val = new System.Windows.Forms.Label();
+            this.trackBar_at = new System.Windows.Forms.TrackBar();
+            this.label_at_val = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_kp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_ki)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_kd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_at)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(12, 12);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(75, 23);
+            this.button_connect.TabIndex = 0;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // listBox_events
+            // 
+            this.listBox_events.FormattingEnabled = true;
+            this.listBox_events.Location = new System.Drawing.Point(594, 12);
+            this.listBox_events.Name = "listBox_events";
+            this.listBox_events.Size = new System.Drawing.Size(409, 433);
+            this.listBox_events.TabIndex = 1;
+            // 
+            // button_motor
+            // 
+            this.button_motor.Location = new System.Drawing.Point(48, 80);
+            this.button_motor.Name = "button_motor";
+            this.button_motor.Size = new System.Drawing.Size(75, 23);
+            this.button_motor.TabIndex = 2;
+            this.button_motor.Text = "Motor";
+            this.button_motor.UseVisualStyleBackColor = true;
+            this.button_motor.Click += new System.EventHandler(this.button_motor_Click);
+            // 
+            // label_angleVal
+            // 
+            this.label_angleVal.AutoSize = true;
+            this.label_angleVal.Location = new System.Drawing.Point(64, 234);
+            this.label_angleVal.Name = "label_angleVal";
+            this.label_angleVal.Size = new System.Drawing.Size(33, 13);
+            this.label_angleVal.TabIndex = 3;
+            this.label_angleVal.Text = "angle";
+            // 
+            // trackBar_kp
+            // 
+            this.trackBar_kp.Location = new System.Drawing.Point(12, 310);
+            this.trackBar_kp.Maximum = 400;
+            this.trackBar_kp.Name = "trackBar_kp";
+            this.trackBar_kp.Size = new System.Drawing.Size(450, 45);
+            this.trackBar_kp.TabIndex = 4;
+            this.trackBar_kp.ValueChanged += new System.EventHandler(this.trackBar_kp_ValueChanged);
+            // 
+            // trackBar_ki
+            // 
+            this.trackBar_ki.Location = new System.Drawing.Point(12, 361);
+            this.trackBar_ki.Maximum = 400;
+            this.trackBar_ki.Name = "trackBar_ki";
+            this.trackBar_ki.Size = new System.Drawing.Size(450, 45);
+            this.trackBar_ki.TabIndex = 4;
+            this.trackBar_ki.ValueChanged += new System.EventHandler(this.trackBar_ki_ValueChanged);
+            // 
+            // trackBar_kd
+            // 
+            this.trackBar_kd.Location = new System.Drawing.Point(12, 412);
+            this.trackBar_kd.Maximum = 400;
+            this.trackBar_kd.Name = "trackBar_kd";
+            this.trackBar_kd.Size = new System.Drawing.Size(450, 45);
+            this.trackBar_kd.TabIndex = 4;
+            this.trackBar_kd.ValueChanged += new System.EventHandler(this.trackBar_kd_ValueChanged);
+            // 
+            // label_kp_val
+            // 
+            this.label_kp_val.AutoSize = true;
+            this.label_kp_val.Location = new System.Drawing.Point(491, 310);
+            this.label_kp_val.Name = "label_kp_val";
+            this.label_kp_val.Size = new System.Drawing.Size(19, 13);
+            this.label_kp_val.TabIndex = 5;
+            this.label_kp_val.Text = "kp";
+            // 
+            // label_ki_val
+            // 
+            this.label_ki_val.AutoSize = true;
+            this.label_ki_val.Location = new System.Drawing.Point(491, 361);
+            this.label_ki_val.Name = "label_ki_val";
+            this.label_ki_val.Size = new System.Drawing.Size(15, 13);
+            this.label_ki_val.TabIndex = 5;
+            this.label_ki_val.Text = "ki";
+            // 
+            // label_kd_val
+            // 
+            this.label_kd_val.AutoSize = true;
+            this.label_kd_val.Location = new System.Drawing.Point(491, 412);
+            this.label_kd_val.Name = "label_kd_val";
+            this.label_kd_val.Size = new System.Drawing.Size(19, 13);
+            this.label_kd_val.TabIndex = 5;
+            this.label_kd_val.Text = "kd";
+            // 
+            // trackBar_at
+            // 
+            this.trackBar_at.Location = new System.Drawing.Point(12, 532);
+            this.trackBar_at.Maximum = 400;
+            this.trackBar_at.Name = "trackBar_at";
+            this.trackBar_at.Size = new System.Drawing.Size(450, 45);
+            this.trackBar_at.TabIndex = 4;
+            this.trackBar_at.ValueChanged += new System.EventHandler(this.trackBar_at_ValueChanged);
+            // 
+            // label_at_val
+            // 
+            this.label_at_val.AutoSize = true;
+            this.label_at_val.Location = new System.Drawing.Point(487, 532);
+            this.label_at_val.Name = "label_at_val";
+            this.label_at_val.Size = new System.Drawing.Size(33, 13);
+            this.label_at_val.TabIndex = 5;
+            this.label_at_val.Text = "angle";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1015, 688);
+            this.Controls.Add(this.label_at_val);
+            this.Controls.Add(this.label_kd_val);
+            this.Controls.Add(this.label_ki_val);
+            this.Controls.Add(this.label_kp_val);
+            this.Controls.Add(this.trackBar_at);
+            this.Controls.Add(this.trackBar_kd);
+            this.Controls.Add(this.trackBar_ki);
+            this.Controls.Add(this.trackBar_kp);
+            this.Controls.Add(this.label_angleVal);
+            this.Controls.Add(this.button_motor);
+            this.Controls.Add(this.listBox_events);
+            this.Controls.Add(this.button_connect);
+            this.Name = "Form1";
+            this.Text = "Remote Control";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_kp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_ki)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_kd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_at)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.ListBox listBox_events;
+        private System.Windows.Forms.Button button_motor;
+        private System.Windows.Forms.Label label_angleVal;
+        private System.Windows.Forms.TrackBar trackBar_kp;
+        private System.Windows.Forms.TrackBar trackBar_ki;
+        private System.Windows.Forms.TrackBar trackBar_kd;
+        private System.Windows.Forms.Label label_kp_val;
+        private System.Windows.Forms.Label label_ki_val;
+        private System.Windows.Forms.Label label_kd_val;
+        private System.Windows.Forms.TrackBar trackBar_at;
+        private System.Windows.Forms.Label label_at_val;
+    }
+}
+
---
+++ RemoteControl/RemoteControl/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace RemoteControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            if (!isConnected())
+            {
+                connect();
+            } else {
+                disconnect();
+            }
+        }
+
+        private bool isConnected()
+        {
+            return port != null;
+        }
+
+        private void update_ui()
+        {
+            if (isConnected())
+            {
+                button_connect.Text = "Disconnect";
+            }
+            else
+            {
+                button_connect.Text = "Connect";
+            }
+
+            trackBar_kp.Value = trackBar_kp.Maximum / 2;
+            trackBar_ki.Value = trackBar_ki.Maximum / 2;
+            trackBar_kd.Value = trackBar_kd.Maximum / 2;
+
+            trackBar_at.Value = trackBar_at.Maximum / 2;
+        }
+
+        private SerialPort port = null;
+        private const string comport = "COM7";
+        private void connect()
+        {
+            try
+            {
+                port = new SerialPort(comport, 9600);
+                port.Parity = Parity.None;
+                port.StopBits = StopBits.One;
+                port.DataBits = 8;
+                port.Handshake = Handshake.None;
+                port.DataReceived += Port_DataReceived;
+                port.Open();
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            listBox_events.Items.Clear();
+            update_ui();
+        }
+
+        private void disconnect()
+        {
+            port.DataReceived -= Port_DataReceived;
+            port.Close();
+            port = null;
+
+            update_ui();
+        }
+
+        private delegate int AddCallDelegate(string text);
+        private void register_event(string msg)
+        {
+            string entry_text = DateTime.Now.ToLongTimeString() + " --- " + msg;
+            
+            var add_entry = new AddCallDelegate(listBox_events.Items.Add);
+            //Invoke(add_entry, new object[] { entry_text });
+            add_entry(entry_text);
+
+            listBox_events.SelectedIndex = listBox_events.Items.Count - 1;
+            listBox_events.SelectedIndex = -1;
+        }
+
+        private void handle_in_data(byte ch, byte[] data, int len)
+        {
+            string msg;
+
+            switch (ch)
+            {
+                case 0:
+                    msg = ASCIIEncoding.ASCII.GetString(data, 0, len);
+                    register_event("inf: " + msg);
+                    break;
+                case 1:
+                    msg = ASCIIEncoding.ASCII.GetString(data, 0, len);
+                    register_event("dbg: " + msg);
+                    break;
+                case 2:
+                    msg = ASCIIEncoding.ASCII.GetString(data, 0, len);
+                    register_event("err: " + msg);
+                    break;
+                case 10:
+                    // a pid data package
+                    UInt32 pid_sampletime = BitConverter.ToUInt32(data, 4 * 0);
+                    UInt32 pid_handlertime = BitConverter.ToUInt32(data, 4 * 1);
+                    UInt32 pid_counter = BitConverter.ToUInt32(data, 4 * 2);
+
+                    float angle = BitConverter.ToSingle(data, 4 * 3);
+                    float error = BitConverter.ToSingle(data, 4 * 4);
+                    float errorDiff = BitConverter.ToSingle(data, 4 * 5);
+                    float errorSum = BitConverter.ToSingle(data, 4 * 6);
+
+                    byte last_noyield = data[4 * 7];
+
+                    label_angleVal.Text = angle.ToString();
+
+                    break;
+                default:
+                    register_event(String.Format("incoming data, unhandled channel: {0}", ch));
+                    break;
+            }
+        }
+
+        private void send_value(byte ch, Int32 val)
+        {
+            send_out_data(ch, BitConverter.GetBytes(val), 4);
+        }
+        private void send_value(byte ch, float val)
+        {
+            send_out_data(ch, BitConverter.GetBytes(val), 4);
+        }
+
+        private byte[] out_buffer = new byte[2 + 1 + 1 + 255 + 2];
+        private void send_out_data(byte ch, byte[] data, int len)
+        {
+            byte lenb = (byte)len;
+            int lenTotal = 2 + 1 + 1 + lenb + 2;
+            out_buffer[0] = 0x55;
+            out_buffer[1] = 0xAA;
+            out_buffer[2] = ch;
+            out_buffer[3] = lenb;
+            Buffer.BlockCopy(data, 0, out_buffer, 4, lenb);
+            out_buffer[lenTotal - 2] = 0x88;
+            out_buffer[lenTotal - 1] = 0x11;
+
+            port.Write(out_buffer, 0, lenTotal);
+            System.Threading.Thread.Sleep(100);
+        }
+
+        private int in_idx = 0;
+        private byte in_ch = 0;
+        private byte in_data_len = 0;
+        private byte[] in_data = new byte[255];
+
+        private delegate void HandleSerialPortDelegate(SerialPort port);
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
+            BeginInvoke(new HandleSerialPortDelegate(in_handle), new object[] { port });
+            /*
+            try
+            {
+                in_handle();
+            }
+            catch (Exception ex)
+            {
+                register_event(ex.Message);
+            }*/
+        }
+
+        private void in_handle(SerialPort port)
+        {
+            try
+            {
+                while (port.BytesToRead > 0)
+                {
+                    int i = port.ReadByte();
+                    if (i < 0)
+                        throw new Exception("stream ended unexpectedly");
+                    byte b = (byte)i;
+
+                    switch (in_idx)
+                    {
+                        case 0:
+                            if (b == 0x55)
+                                in_idx++;
+                            continue;
+                        case 1:
+                            if (b == 0xAA)
+                            {
+                                in_idx++;
+                            }
+                            else
+                            {
+                                in_idx = 0;
+                                register_event("ERR: comm init sync");
+                            }
+                            continue;
+                        case 2: // channel
+                            in_idx++;
+                            in_ch = b;
+                            continue;
+                        case 3: // length
+                            in_idx++;
+                            in_data_len = b;
+                            continue;
+                        default:
+                            break;
+                    }
+
+                    int in_data_idx = in_idx - 4;
+                    int in_data_after_idx = in_data_idx - in_data_len;
+                    if (in_data_after_idx < 0)
+                    {
+                        in_idx++;
+                        in_data[in_data_idx] = b;
+                    }
+                    else if (in_data_after_idx == 0)
+                    {
+                        if (b == 0x88)
+                        {
+                            in_idx++;
+                        }
+                        else
+                        {
+                            in_idx = 0;
+                            register_event("ERR: comm end sync");
+                        }
+                    }
+                    else if (in_data_after_idx == 1)
+                    {
+                        if (b == 0x11)
+                        {
+                            in_idx = 0;
+                            handle_in_data(in_ch, in_data, in_data_len);
+                        }
+                        else
+                        {
+                            in_idx = 0;
+                            register_event("ERR: comm end sync");
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("unexpected comm parsing state");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                register_event(ex.Message);
+            }
+        }
+
+        private bool motor_on = false;
+        private void button_motor_Click(object sender, EventArgs e)
+        {
+            motor_on = !motor_on;
+            send_value(50, motor_on ? (int)0 : (int)1);
+        }
+
+        private void trackBar_kp_ValueChanged(object sender, EventArgs e)
+        {
+            if (port == null) return;
+            float value = ((float)trackBar_kp.Value) / (trackBar_kp.Maximum / 2) - 1;
+
+            float kp = 0.15f;
+            kp += value * 0.1f;
+
+            label_kp_val.Text = kp.ToString();
+
+            send_value(60, kp);
+        }
+
+        private void trackBar_ki_ValueChanged(object sender, EventArgs e)
+        {
+            if (port == null) return;
+            float value = ((float)trackBar_ki.Value) / (trackBar_ki.Maximum / 2) - 1;
+
+            float ki = 0.9f;
+            ki += value * 0.6f;
+
+            label_ki_val.Text = ki.ToString();
+
+            send_value(61, ki);
+        }
+
+        private void trackBar_kd_ValueChanged(object sender, EventArgs e)
+        {
+            if (port == null) return;
+            float value = ((float)trackBar_kd.Value) / (trackBar_kd.Maximum / 2) - 1;
+
+            float kd = 0.003f;
+            kd += value * 0.002f;
+
+            label_kd_val.Text = kd.ToString();
+
+            send_value(62, kd);
+        }
+
+        private void trackBar_at_ValueChanged(object sender, EventArgs e)
+        {
+            if (port == null) return;
+            float value = ((float)trackBar_at.Value) / (trackBar_at.Maximum / 2) - 1;
+
+            float at = 6.2f;
+            at += value * 2.0f;
+
+            label_at_val.Text = at.ToString();
+
+            send_value(70, at);
+        }
+    }
+}
