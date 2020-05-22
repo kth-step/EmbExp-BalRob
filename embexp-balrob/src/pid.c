62656a106cf8a50c8d1e072bad9f22df50594583
Andreas Lindner <andili@kth.se>
2020-05-23 00:45:08 +0200

    Unify

--- embexp-balrob/src/pid.c
+++ embexp-balrob/src/pid.c
@@ -70,12 +70,7 @@ uint8_t KEEPINFLASH pid_msg_read(pid_msg_t* m) {
 
 
 // inputs
-#ifdef BOT_LEGO
-volatile uint8_t motor_on = 1;
-#endif
-#ifdef BOT_MINI
 volatile uint8_t motor_on = 0;
-#endif
 volatile float angleTarget = ANGLETARGET;
 volatile float kp = INIT_KP;
 volatile float ki = INIT_KI;
@@ -274,14 +269,12 @@ void KEEPINFLASH pid() {
 		int in_ch;
 
 		// handle button, hope that the loop is so slow that it is already debounced
-#ifdef BOT_MINI
 		uint8_t button = ui_get_button();
 		if (button != button_last && button == 0) {
 			motor_on = !motor_on;
 			out_info("button!");
 		}
 		button_last = button;
-#endif
 
 		// handle io
 		while ((in_ch = in_handle()) == -3);
