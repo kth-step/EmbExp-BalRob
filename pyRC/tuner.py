d03dabb8d1b25a7fa4700d19a2e541b85ec6619a
Andreas Lindner <andili@kth.se>
2020-06-11 14:26:29 +0200

    Something

--- pyRC/tuner.py
+++ pyRC/tuner.py
@@ -13,18 +13,19 @@ from tkinter import *
 
 with serial.Serial(balrob.serialdevice, 9600, timeout=None) as ser:
 	master = Tk()
-	kp_scale = Scale(master, from_=-0.1, to=0.1, resolution=0.0002, orient=HORIZONTAL, length=1000)
+	factor = 2
+	kp_scale = Scale(master, from_=-0.1 * factor, to=0.1 * factor, resolution=0.0002 * factor, orient=HORIZONTAL, length=1000)
 	kp_scale.set(0.0)
-	ki_scale = Scale(master, from_=-0.1, to=0.1, resolution=0.0002, orient=HORIZONTAL, length=1000)
+	ki_scale = Scale(master, from_=-0.1 * factor, to=0.1 * factor, resolution=0.0002 * factor, orient=HORIZONTAL, length=1000)
 	ki_scale.set(0.0)
-	kd_scale = Scale(master, from_=-0.01, to=0.01, resolution=0.00002, orient=HORIZONTAL, length=1000)
+	kd_scale = Scale(master, from_=-0.01 * factor, to=0.01 * factor, resolution=0.00002 * factor, orient=HORIZONTAL, length=1000)
 	kd_scale.set(0.0)
 	angle_scale = Scale(master, from_=-1.0, to=1.0, resolution=0.002, orient=HORIZONTAL, length=1000)
 	angle_scale.set(0.0)
 
-	kp_scale.set(0.0784)
-	ki_scale.set(0.0554)
-	kd_scale.set(0.00172)
+	kp_scale.set(0.0756)
+	ki_scale.set(0.1200)
+	kd_scale.set(0.00252)
 
 	def rob_motor_on():
 		balrob.set_motor(ser, True)
