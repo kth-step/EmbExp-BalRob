6af8b0760b70d57644bc948f8307ef01d3dad0d3
Andreas Lindner <andili@kth.se>
2021-05-13 15:15:59 +0200

    Add input setter functions

--- embexp-balrob/src/pid.c
+++ embexp-balrob/src/pid.c
@@ -157,6 +157,29 @@ void KEEPINFLASH imu_handler_pid_entry_empty(uint8_t noyield, uint32_t pid_sampl
 							   .last_noyield = noyield});
 }
 
+void imu_handler_pid_set_state_PID(float __kp, float __ki, float __kd, float __angleLast, float __errorLast, float __errorSum) {
+  kp = __kp;
+  ki = __ki;
+  kd = __kd;
+
+  angleLast = __angleLast;
+  errorLast = __errorLast;
+  errorSum  = __errorSum;
+}
+void imu_handler_pid_set_state_INPUT(uint8_t __msg_flag, uint8_t __motor_on, float __angleTarget, uint32_t __pid_counter) {
+  msg_flag = __msg_flag;
+  motor_on = __motor_on;
+
+  angleTarget = __angleTarget;
+  pid_counter = __pid_counter;
+}
+void imu_handler_pid_set_state_IMU(int16_t __accX, int16_t __accZ, int16_t __gyrY) {
+  imu_values[0] = __accX;
+  imu_values[2] = __accZ;
+
+  imu_values[5] = __gyrY;
+}
+
 void imu_handler_pid_entry(uint8_t noyield, uint32_t pid_sampletime) {
 
 	// pick out the relevant imu values
